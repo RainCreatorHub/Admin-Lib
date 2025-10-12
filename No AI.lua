893e044f798a55ec6846d9788bd40f0db5822799
RainCreatorHub <rain.hub.creator@gmail.com>
2025-10-12 07:00:55 -0300

    Update No AI.lua

--- No AI.lua
+++ No AI.lua
@@ -6,18 +6,19 @@ function Lib:Window(Info)
   local CoreGui = game:GetService("CoreGui")
 
   local S = Instance.new("ScreenGui")
-    S.ResetOnSpawn = false
-    S.Parent = CoreGui
+  S.ResetOnSpawn = false
+  S.Parent = CoreGui
 
   local Win = Instance.new("Frame")
-    Win.Size = UDim2.new(0, Info.Size[1] or 470, 0, Info.Size[2] or 340)
-    Win.Position = UDim2.new(0, 200, 0, 15)
-    Win.Parent = S
+  Win.Size = UDim2.new(0, Info.Size[1] or 470, 0, Info.Size[2] or 340)
+  Win.Position = UDim2.new(0.5, -235, 0.5, -170)
+  Win.Parent = S
 
   function WinOb:Label(Info)
-  
+
   end
- return WinOb
+
+  return WinOb
 end
 
 return Lib
