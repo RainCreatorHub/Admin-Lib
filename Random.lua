4f6f27fb4c9add769f9d67f0129fe9943e64100f
RainCreatorHub <rain.hub.creator@gmail.com>
2025-10-12 22:30:59 -0300

    Update Random.lua

--- Random.lua
+++ Random.lua
@@ -1,29 +1,48 @@
 local Lib = {}
 local CoreGui = game:GetService("CoreGui")
 
+if CoreGui:FindFirstChild("AdL") then
+  CoreGui:FindFirstChild("AdL"):Destroy()
+end
+
+task.wait(1)
 function Lib:Window(Info)
     local Info = Info or {}
     
     local S = Instance.new("ScreenGui")
+    S.Name = "AdL"
     S.ResetOnSpawn = false
     S.Parent = CoreGui
 
     local Win = Instance.new("Frame")
-    Win.Size = UDim2.new(0, Info.Size[1] or 470, 0, Info.Size[2] or 340)
-    Win.Position = UDim2.new(0, -100, 0, 15)
+    Win.Size = UDim2.new(0, Info.Size[1] or 450, 0, Info.Size[2] or 320)
+    Win.Position = UDim2.new(0, 200, 0, 15)
     Win.Parent = S
 
+    local BWin = Instance.new("UICorner")
+    BWin.Parent = Win
+
     local Tbar = Instance.new("Frame")
+    Tbar.Size = UDim2.new(0, 450, 0, 25)
     Tbar.Parent = Win
+
+    local TbarD = Instance.new("Frame")
+    TbarD.Size = UDim2.new(0, 450, 0, 0)
+    TbarD.Position = UDim2.new(0, 0, 0, 25)
+    TbarD.Parent = Win
+
+    local TC = Instance.new("UICorner")
+    TC.Parent = Tbar
 end
 
 function Lib:Demo()
   local Window = Lib:Window({
-        Size = {
-            470,
-            340
-        }
+    Size = {
+     450,
+     320
+    }
   })
 end
 
+Lib:Demo()
 return Lib
